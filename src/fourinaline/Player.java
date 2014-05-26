b844d32857ab6eafc08b2f61b184da27624c9e1a
Jon <jonhuang91@yahoo.com>
2014-05-26 14:13:34 -0700

    Player class complete???

--- src/fourinaline/Player.java
+++ src/fourinaline/Player.java
@@ -1,9 +1,39 @@
 package fourinaline;
+import java.util.*;
 
 class Player
 {
-
-	
-	char piece = 'O';
-
+	private char piece = 'O';
+	public Player(Board b, String s)
+	{
+		int row = findIndexLetter(s.charAt(0));
+		int col = findIndex(s.charAt(1));
+		b.setPiece(piece,row,col);
+	}
+	//return -1 if invalid input
+	public int findIndexLetter(char ch)
+	{
+		if(Character.toLowerCase(ch)=='a')    {return 0;}
+		if(Character.toLowerCase(ch)=='b')    {return 1;}
+		if(Character.toLowerCase(ch)=='c')    {return 2;}
+		if(Character.toLowerCase(ch)=='d')    {return 3;}
+		if(Character.toLowerCase(ch)=='e')    {return 4;}
+		if(Character.toLowerCase(ch)=='f')    {return 5;}
+		if(Character.toLowerCase(ch)=='g')    {return 6;}
+		if(Character.toLowerCase(ch)=='h')    {return 7;}
+		return -1;
+	}
+	//return -1 if invalid input
+	public int findIndex(char ch)
+	{
+		if(Character.getNumericValue(ch)==1)   {return 0;}
+		if(Character.getNumericValue(ch)==2)   {return 1;}
+		if(Character.getNumericValue(ch)==3)   {return 2;}
+		if(Character.getNumericValue(ch)==4)   {return 3;}
+		if(Character.getNumericValue(ch)==5)   {return 4;}
+		if(Character.getNumericValue(ch)==6)   {return 5;}
+		if(Character.getNumericValue(ch)==7)   {return 6;}
+		if(Character.getNumericValue(ch)==8)   {return 7;}
+		return -1;
+	}
 }

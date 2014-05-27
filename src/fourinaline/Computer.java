2df124b295cc3dd4eff9a4635ea901763dbdb9d9
cwlei <cwlei@users.noreply.github.com>
2014-05-26 18:29:13 -0700

    Added basic structure for Computer

--- src/fourinaline/Computer.java
+++ src/fourinaline/Computer.java
@@ -1,5 +1,28 @@
 package fourinaline;
 
-public class Computer {
+public class Computer extends Player
+{
+	private String piece = "O";
+	private Board computerB;
+	
+	public Computer(Board b)
+	{
+		super(b);
+		computerB = b;
+	}
+	
+	public void makeMove()
+	{
+		String s = alphaBeta();
+		int row = findIndexLetter(s.charAt(0));
+		int col = findIndex(s.charAt(1));
+		computerB.setPiece(piece, row, col);
+	}
+	
+	private String alphaBeta()
+	{
+		String move = "";
+		return move;
+	}
 
 }
